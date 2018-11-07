f35456b9f7372fe7d99f62e5673ec1a76bd37cbb
Russ Hammett <Kritner@gmail.com>
2018-11-06 19:42:04 -0500

    IList.AddIfNotNull

---
+++ src/Kritner.ExtensionMethods/ListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kritner.ExtensionMethods
+{
+    /// <summary>
+    /// <see cref="IList{T}"/> extensions.
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Add object to <see cref="IList{T}"/> only when not null.
+        /// </summary>
+        /// <typeparam name="T">The type contained within the <see cref="List{T}"/>.</typeparam>
+        /// <param name="obj">The <see cref="List{T}"/>.</param>
+        /// <param name="item">The item to potentially add.</param>
+        public static void AddIfNotNull<T>(this IList<T> obj, T item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            obj.Add(item);
+        }
+    }
+}
---
+++ test/Kritner.ExtensionMethods.Tests/ListExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Kritner.ExtensionMethods.Tests
+{
+    public class ListExtensionTests
+    {
+        private class TestClass { }
+
+        #region AddIfNotNull
+        [Fact]
+        public void ShouldAddItemWhenNotNull()
+        {
+            var list = new List<TestClass>();
+            var notNullItem = new TestClass();
+
+            list.AddIfNotNull(notNullItem);
+
+            Assert.True(list.Count == 1);
+        }
+
+        [Fact]
+        public void ShouldNotAddItemWhenNull()
+        {
+            var list = new List<TestClass>();
+            TestClass nullItem = null;
+
+            list.AddIfNotNull(nullItem);
+
+            Assert.True(list.Count == 0);
+        }
+        #endregion AddIfNotNull
+    }
+}
