3b28b79ac6959c3c2e43b7bbb02e1fcf32cfc021
Russ Hammett <Kritner@gmail.com>
2018-11-06 21:44:56 -0500

    AddRangeIfNotNull

--- src/Kritner.ExtensionMethods/ListExtensions.cs
+++ src/Kritner.ExtensionMethods/ListExtensions.cs
@@ -12,8 +12,8 @@ namespace Kritner.ExtensionMethods
         /// <summary>
         /// Add object to <see cref="IList{T}"/> only when not null.
         /// </summary>
-        /// <typeparam name="T">The type contained within the <see cref="List{T}"/>.</typeparam>
-        /// <param name="obj">The <see cref="List{T}"/>.</param>
+        /// <typeparam name="T">The type contained within the <see cref="IList{T}"/>.</typeparam>
+        /// <param name="obj">The <see cref="IList{T}"/>.</param>
         /// <param name="item">The item to potentially add.</param>
         /// <exception cref="ArgumentNullException">Thrown if <see cref="obj"/> is null</exception>
         public static void AddIfNotNull<T>(this IList<T> obj, T item)
@@ -29,6 +29,37 @@ namespace Kritner.ExtensionMethods
             }
 
             obj.Add(item);
+        }
+
+        /// <summary>
+        /// Adds a <see cref="IEnumerable{T}"/> to an <see cref="IList{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type contained within the <see cref="IList{T}"/>.</typeparam>
+        /// <param name="obj">The <see cref="IList{T}"/>.</param>
+        /// <param name="item">The items to potentially add.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <see cref="obj"/> is null</exception>
+        public static void AddRangeIfNotNull<T>(this IList<T> obj, IEnumerable<T> items)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException($"{nameof(obj)} is null");
+            }
+
+            if (items == null)
+            {
+                return;
+            }
+
+            if (obj is List<T> objList)
+            {
+                objList.AddRange(items);
+                return;
+            }
+
+            foreach (var item in items)
+            {
+                obj.Add(item);
+            }
         }
     }
 }
--- test/Kritner.ExtensionMethods.Tests/ListExtensionTests.cs
+++ test/Kritner.ExtensionMethods.Tests/ListExtensionTests.cs
@@ -6,11 +6,11 @@ namespace Kritner.ExtensionMethods.Tests
 {
     public class ListExtensionTests
     {
-        private class TestClass { }
+        public class TestClass { }
 
         #region AddIfNotNull
         [Fact]
-        public void ShouldAddItemWhenNotNull()
+        public void AddIfNotNull_ShouldAddItemWhenNotNull()
         {
             var list = new List<TestClass>();
             var notNullItem = new TestClass();
@@ -21,7 +21,7 @@ namespace Kritner.ExtensionMethods.Tests
         }
 
         [Fact]
-        public void ShouldNotAddItemWhenNull()
+        public void AddIfNotNull_ShouldNotAddItemWhenNull()
         {
             var list = new List<TestClass>();
             TestClass nullItem = null;
@@ -32,7 +32,7 @@ namespace Kritner.ExtensionMethods.Tests
         }
 
         [Fact]
-        public void ShouldThrowIfListIsNull()
+        public void AddIfNotNull_ShouldThrowIfListIsNull()
         {
             List<TestClass> list = null;
             var item = new TestClass();
@@ -40,5 +40,81 @@ namespace Kritner.ExtensionMethods.Tests
             Assert.Throws<ArgumentNullException>(() => list.AddIfNotNull(item));
         }
         #endregion AddIfNotNull
+
+        #region AddRangeIfNotNull
+        public static IEnumerable<object[]> AddRangeSuccessMultipleType =>
+            new List<object[]>()
+            {
+                new object[]
+                {
+                    new TestClass[] { new TestClass(), }, 1
+                },
+                new object[] 
+                {
+                    new TestClass[] { new TestClass(), new TestClass() }, 2
+                },
+                new object[]
+                {
+                    new List<TestClass> { new TestClass(), }, 1
+                },
+                new object[]
+                {
+                    new List<TestClass> { new TestClass(), new TestClass() }, 2
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(AddRangeSuccessMultipleType))]
+        public void AddRangeIfNotNull_ShouldAddItemsWhenNotNull(
+            IEnumerable<TestClass> testData, 
+            int numberOfElementsToAdd
+        )
+        {
+            var list = new List<TestClass>();
+
+            list.AddRangeIfNotNull(testData);
+
+            Assert.Equal(numberOfElementsToAdd, list.Count);
+        }
+
+        public static IEnumerable<object[]> AddRangeNoItemsMultipleType =>
+            new List<object[]>()
+            {
+                new object[]
+                {
+                    new TestClass[] { }
+                },
+                new object[]
+                {
+                    null
+                },
+                new object[]
+                {
+                    new List<TestClass> ()
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(AddRangeNoItemsMultipleType))]
+        public void AddRangeIfNotNull_ShouldNotAddItemsWhenNoItemsToAdd(IEnumerable<TestClass> testData)
+        {
+            var list = new List<TestClass>();
+
+            list.AddRangeIfNotNull(testData);
+
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void AddRangeIfNotNull_ShouldThrowIfListIsNull()
+        {
+            List<TestClass> list = null;
+            var item = new TestClass();
+
+            Assert.Throws<ArgumentNullException>(
+                () => list.AddRangeIfNotNull(new[] { item })
+            );
+        }
+        #endregion AddRangeIfNotNull
     }
 }
