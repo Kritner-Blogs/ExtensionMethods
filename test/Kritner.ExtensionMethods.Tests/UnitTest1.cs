090bb30d26808e1b1c4b908542175f9ed54f2413
Russ Hammett <Kritner@gmail.com>
2018-11-06 19:27:00 -0500

    Project stubs

---
+++ test/Kritner.ExtensionMethods.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Kritner.ExtensionMethods.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
