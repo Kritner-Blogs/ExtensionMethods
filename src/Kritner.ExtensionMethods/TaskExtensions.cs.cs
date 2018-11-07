2d7136875a34485948c568e7ac2f1aa7ae6bcacf
Russ Hammett <Kritner@gmail.com>
2018-11-07 18:18:42 -0500

    IEnumerable extension TryFirst w/ tests

---
+++ src/Kritner.ExtensionMethods/TaskExtensions.cs.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Kritner.ExtensionMethods
+{
+    /// <summary>
+    /// Extension Methods for <see cref="Task"/>
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Can be used to fire off a Task and not await the reuslt.
+        /// </summary>
+        /// <param name="task"></param>
+        public static void FireAndForget(this Task task) { }
+    }
+}
